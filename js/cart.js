84ac09a2173a8cb4f5ac37008bb8c976e1138d66
Jumperenterprise <andrew.jumper.ent@gmail.com>
2023-03-29 23:05:43 +0100

    Page Product, Cart

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,31 @@
 const list = JSON.parse(localStorage.getItem("list"));
+const img = document.getElementById("img")
 
 
 function getProductsCart() {
 
     let [product] = list
 
-    console.log(product)
-
     getProducts().then(items => {     
-      const result = items.find((item) => {items.productId === item._id});
-      console.log(result)           
+      console.log(items)
+      for(let i = 0; i < items.length; i++) {
+        const elements = items[i];
+        const result = elements._id == product.productId
+        if(result) {
+          const panier = [          
+            elements._id,
+            elements.name,
+            elements.price,
+            elements.imageUrl,
+            elements.description
+          ]
+
+          img.setAttribute('src', elements.imageUrl)
+          
+        }
+
+        console.log(result)
+      }         
     })
 }
 
